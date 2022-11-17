4149b1715da3122d5d95e96f76a4ffa1c9b9bd32
amirny2205 <amirny2205@yandex.ru>
2022-11-17 23:43:30 +0300

    buy_item_works

--- stripe_simple/views.py
+++ stripe_simple/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import JsonResponse
 from .models import Item
 # Create your views here.
 
 
 def get_session(request, pk):
-    return HttpResponse('sdasjkdjl;as')
+    return JsonResponse({'id': 'sdasjkdjl;as'})
 
 
 def buy_item(request, pk):
